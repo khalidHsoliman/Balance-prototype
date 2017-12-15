fdccdd219a1c97f6c1a015d999ab4bdea31bbbab
Khalid Hassan <khalidhsoliman@gmail.com>
2017-12-15 23:28:25 +0200

    add dataBase connection code

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,8 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Data;
+using Mono.Data.SqliteClient; 
+
 
 public class gameManager : Singleton<gameManager> {
 
@@ -27,6 +28,34 @@ public class gameManager : Singleton<gameManager> {
 
     private void Start()
     {
+        //DataBase connection 
+
+        string conn = "URI=file:" + Application.dataPath + "/PickAndPlaceDatabase.s3db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT value,name, randomSequence " + "FROM PlaceSequence";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int value = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            int rand = reader.GetInt32(2);
+
+            Debug.Log("value= " + value + "  name =" + name + "  random =" + rand);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+
+        //
+
+
         gameIsOver = false;
 
         score = 0;
@@ -73,7 +102,7 @@ public class gameManager : Singleton<gameManager> {
 
     public void endGame()
     {
-        gameIsOver = true; 
+        gameIsOver = true;
 
         if(sceneLight)
             sceneLight.gameObject.SetActive(false);
